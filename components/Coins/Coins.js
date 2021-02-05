85bba940a7e63d64a8352ed341684c34e240488b
Adithya Pai B <paiadithya26@gmail.com>
2021-02-05 21:52:30 +0530

    Added responsivness

--- components/Coins/Coins.js
+++ components/Coins/Coins.js
@@ -18,41 +18,32 @@ const Coins = ({
       <Link href="/coin/[id]" as={`/coin/${id}`}>
         <a>
           <div className={styles.coin_row}>
-         
-              <img src={image} alt={name} className={styles.coin_img} />
-              <ul className={styles.list}>
-              <h1 className={styles.coin_h1}>{name}</h1>
-              <h5 className={styles.coin_symbol}>{symbol}</h5>
-              </ul>
-              
-           
-            <div className={styles.coin_data}>
+            <div className={styles.coin_header}>
+            <img src={image} alt={name} className={styles.coin_img} />
             <ul className={styles.list}>
+              <h4 className={styles.coin_h4}>{name}</h4>
+              <p className={styles.coin_symbol}>{symbol}</p>
+            </ul>
+            </div>  
+            <div className={styles.coin_data}>
+              <ul className={styles.list}>
                 <li>Percentage</li>
                 <li>
-                  {" "}
-                  {priceChange < 0 ? (
-                    <p className={(styles.coin_percent, styles.red)}>
-                      - {priceChange.toFixed(2)}%
-                    </p>
-                  ) : (
-                    <p className={(styles.coin_percent, styles.green)}>
-                      + {priceChange.toFixed(2)}%
-                    </p>
-                  )}
+                  {priceChange < 0 ? (<p className={(styles.coin_percent, styles.red)}>{priceChange.toFixed(2)}%</p>) : (
+                    <p className={(styles.coin_percent, styles.green)}>+ {priceChange.toFixed(2)}%</p>)}
                 </li>
               </ul>
-              <ul>
-                <p className={styles.coin_price}>Price</p>
-                <p className={styles.coin_price}>${price}</p>
+              <ul className={styles.list}>
+                <li className={styles.coin_price}>Price</li>
+                <li className={styles.coin_price}>${price}</li>
               </ul>
-              <p className={styles.coin_marketcap}>
-                24h
+            
                 <ul className={styles.list}>
+                <li className={styles.black}>24h</li>
                   <li className={styles.green}>High {high.toLocaleString()}</li>
                   <li className={styles.red}>Low {low.toLocaleString()}</li>
                 </ul>
-              </p>
+            
             </div>
           </div>
         </a>
